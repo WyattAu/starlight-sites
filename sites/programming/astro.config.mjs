6283fe7de3ae2b463bd733ae5a43e22b1e697c71
WyattAu <wyatt_au@protonmail.com>
2026-07-11 11:29:21 +0100

    fix: set defaultLocale to 'root' for zh locale config

--- sites/programming/astro.config.mjs
+++ sites/programming/astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
         MarkdownContent: './src/components/starlight/MarkdownContent.astro',
         Search: './src/components/starlight/Search.astro',
       },
-      defaultLocale: 'en',
+      defaultLocale: 'root',
       locales: {
         root: { label: 'English', lang: 'en' },
         zh: { label: '简体中文', lang: 'zh' },
